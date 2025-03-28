9655d7faf7a14f667c407db0f0a216fae6f48e8e
Ana G <ac.generoso@gmail.com>
2025-03-28 14:28:12 -0300

    Atualiza arquivos rastreados conforme .gitignore

--- Backup/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "🔧 Atualizando pip, setuptools e wheel..."
-pip install --upgrade pip setuptools wheel
-
-echo "📦 Instalando todos os pacotes..."
-pip install -r requirements.txt
-
